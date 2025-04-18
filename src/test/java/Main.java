36cd970c677a877c1d8c1acb5a51f667fd6c6591
Akamal Aralov <akmalaralov77@gmail.com>
2025-04-17 20:58:29 -0500

    feature1 updated

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("hello world");
-
-        }
-        System.out.println("I am doing changes for v1 branch!");
-        System.out.println("i switched from main to v1!");
+        System.out.println("Hello guys 10 times");
+        System.out.println("Hello guys 10 times");
+        System.out.println("Hello guys 10 times");
+        System.out.println("Hello guys 10 times");
+        System.out.println("Hello guys 10 times");
+        System.out.println("Hello guys 10 times");
+        System.out.println("Hello guys 10 times");
 
     }
 }
